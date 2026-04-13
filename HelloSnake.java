924cb5bbdd19ff3a124900198553e7617c4fb254
reinkimberly <reinkimberly1@gmail.com>
2026-04-13 12:31:06 -0500

    Add HelloSnake.java setup check

---
+++ HelloSnake.java
@@ -0,0 +1,6 @@
+public class HelloSnake {
+    public static void main(String[] args) {
+        System.out.println("Hello, Snake!");
+        System.out.println("This repository can compile Java.");
+    }
+}
